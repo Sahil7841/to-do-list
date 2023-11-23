bfdb09bc828a774d39b3504f4b37f442310c2f32
Sahil7841 <sahil.dalal@ayasya.com>
2023-11-23 13:13:18 +0530

    updated js for  to do list

--- to-do-list.js
+++ to-do-list.js
@@ -1,10 +1,14 @@
 const inputField = document.getElementById("input-field");
 const addBtn = document.getElementById("add-btn");
 const listItems = document.getElementById("list-items");
+const totalCountElement = document.getElementById("total-count");
+const showCompletedCheckbox = document.getElementById("checked");
+// const checkboxDiv = document.querySelector(".checkbox");
 const dataArray = [];
 
-
 addBtn.addEventListener("click", addItem);
+
+totalCountElement.style.display = "none";
 
 function addItem() {
     const inputValue = inputField.value;
@@ -12,6 +16,16 @@ function addItem() {
     if (inputValue === "") {
         alert("Please enter a title.");
         return;
+    }
+
+    totalCountElement.style.display = "block";
+
+    // if (dataArray.length === 0) {
+    //     checkboxDiv.style.display = "block";
+    // }
+
+    function updateTotalCount() {
+        totalCountElement.textContent = dataArray.length;
     }
 
     const checkBox = document.createElement("input");
@@ -33,8 +47,22 @@ function addItem() {
     listItem.appendChild(removeButton);
     listItem.appendChild(editButton);
     listItems.appendChild(listItem);
-    dataArray.push(inputValue);
+
+    dataArray.push({ value: inputValue, completed: false });
+
     inputField.value = "";
+    updateTotalCount();
+
+    checkBox.addEventListener("click", function () {
+        const index = dataArray.findIndex((item) => item.value === updatedValue);
+        if (index !== -1) {
+            dataArray[index].completed = checkBox.checked;
+            updateTotalCount();
+        }
+        toggleCompletedVisibility();
+    });
+    // const arrayData = JSON.stringify(dataArray)
+    //     localStorage.setItem("todolist", arrayData)
 
 
     removeButton.addEventListener("click", function () {
@@ -42,6 +70,8 @@ function addItem() {
         if (index !== -1) {
             dataArray.splice(index, 1);
             listItems.removeChild(listItem);
+            updateTotalCount();
+            toggleCompletedVisibility();
         }
 
     });
@@ -69,6 +99,21 @@ function addItem() {
 
         });
 
+        // saveButton.addEventListener("click", function () {
+        //     const updatedValue = editableInput.value;
+        //     const index = dataArray.findIndex((i) => i.value === inputValue);
+
+        //     if (index !== -1) {
+        //         dataArray[index].value = updatedValue;
+        //         dataArray[index].completed = checkBox.checked; // Update completion status
+        //     }
+
+        //     inputItem.textContent = updatedValue;
+        //     listItem.replaceChild(inputItem, editableInput);
+        //     listItem.removeChild(saveButton);
+        //     inputValue = updatedValue; // Update inputValue with the new value
+        //     toggleCompletedVisibility(); // Update visibility based on completion status
+        // });
 
     });
 
@@ -82,9 +127,139 @@ function addItem() {
 
             if (index !== -1) {
                 dataArray.splice(index, 1);
+                updateTotalCount();
             }
 
             listItems.removeChild(listItem);
         }
     });
+
+    function toggleCompletedVisibility() {
+        const completedItems = dataArray.filter((item) => item.completed);
+        listItems.style.display = showCompletedCheckbox.checked || completedItems.length === 0 ? "block" : "none";
+    }
+
+    showCompletedCheckbox.addEventListener("change", function () {
+        toggleCompletedVisibility();
+    });
+
+
+    function toggleCompletedVisibility() {
+        listItems.innerHTML = "";
+
+        if (showCompletedCheckbox.checked) {
+            const completedItems = dataArray.filter((item) => item.completed);
+            displayItems(completedItems);
+        } else {
+            displayItems(dataArray);
+        }
+    }
+
+    function displayItems(items) {
+        items.forEach((item) => {
+            const listItem = document.createElement("li");
+            const checkBox = document.createElement("input");
+            checkBox.type = "checkbox";
+            const inputItem = document.createElement("span");
+            inputItem.textContent = item.value;
+            const removeButton = document.createElement("button");
+            removeButton.textContent = "Remove";
+            removeButton.classList.add("remove-btn");
+            const editButton = document.createElement("button");
+            editButton.textContent = "Edit";
+            editButton.classList.add("edit-btn");
+
+            listItem.appendChild(checkBox);
+            listItem.appendChild(inputItem);
+            listItem.appendChild(removeButton);
+            listItem.appendChild(editButton);
+            listItems.appendChild(listItem);
+
+            checkBox.checked = item.completed;
+
+            checkBox.addEventListener("click", function () {
+                const index = dataArray.findIndex((i) => i.value === item.value);
+                if (index !== -1) {
+                    dataArray[index].completed = checkBox.checked;
+                    updateTotalCount();
+                }
+                toggleCompletedVisibility();
+            });
+
+            removeButton.addEventListener("click", function () {
+                const index = dataArray.findIndex((i) => i.value === item.value);
+                if (index !== -1) {
+                    dataArray.splice(index, 1);
+                    updateTotalCount();
+                    toggleCompletedVisibility();
+                }
+            });
+
+
+            editButton.addEventListener("click", function () {
+                const editableInput = document.createElement("input");
+                editableInput.type = "text";
+                const saveButton = document.createElement("button");
+                saveButton.textContent = "Save";
+                saveButton.classList.add("save-btn");
+                editableInput.value = inputItem.textContent;
+
+                listItem.replaceChild(editableInput, inputItem);
+                listItem.insertBefore(saveButton, listItem.children[2]);
+                editableInput.focus();
+
+
+                saveButton.addEventListener("click", function () {
+                    const updatedValue = editableInput.value;
+                    dataArray[dataArray.indexOf(inputValue)] = updatedValue;
+                    inputItem.textContent = updatedValue;
+                    listItem.replaceChild(inputItem, editableInput);
+                    listItem.removeChild(saveButton);
+                    inputValue = updatedValue;
+
+                });
+
+
+                //     // saveButton.addEventListener("click", function () {
+                //     //     const updatedValue = editableInput.value;
+                //     //     const index = dataArray.findIndex((i) => i.value === inputValue);
+
+                //     //     if (index !== -1) {
+                //     //         dataArray[index].value = updatedValue;
+                //     //         dataArray[index].completed = checkBox.checked; // Update completion status
+                //     //     }
+
+                //     //     inputItem.textContent = updatedValue;
+                //     //     listItem.replaceChild(inputItem, editableInput);
+                //     //     listItem.removeChild(saveButton);
+                //     //     inputValue = updatedValue; // Update inputValue with the new value
+                //     //     toggleCompletedVisibility(); // Update visibility based on completion status
+                //     // });
+
+
+            });
+
+            listItems.addEventListener("click", function (event) {
+                const target = event.target;
+
+                if (target.classList.contains("remove-btn")) {
+                    const listItem = target.parentElement;
+                    const listItemText = listItem.querySelector("span").textContent.trim();
+                    const index = dataArray.indexOf(listItemText);
+
+                    if (index !== -1) {
+                        dataArray.splice(index, 1);
+                        updateTotalCount();
+                    }
+
+                    listItems.removeChild(listItem);
+                }
+            });
+        });
+    }
+
+    showCompletedCheckbox.addEventListener("change", function () {
+        toggleCompletedVisibility();
+    });
 }
+
